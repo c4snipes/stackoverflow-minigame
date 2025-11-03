05a719016094546d47a1c7872c48a0feefee30c5
Cole Snipes <cole.snipes@icloud.com>
2025-11-03 14:43:54 -0500

    Initialize game core in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
+using stackoverflow_minigame.Game;
 
+namespace stackoverflow_minigame
+{
+    class Program
+    {
+        static void Main()
+        {
+            GameCore game = new GameCore();
+            game.Run();
+        }
+    }
+}
